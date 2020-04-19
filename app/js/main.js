ce6cce9a756772bec362a22dea1a9b036dcd706d
Guangqi Sun <sun357@alumni.purdue.edu>
2020-04-19 15:29:02 -0500

    Balance

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+var confirmBtn = document.getElementById("confirmBtn")
+var totalHr = document.getElementById("totalHr")
+var balance = document.getElementById("balance")
+confirmBtn.addEventListener('click', function() {
+    var startingTime = document.getElementById("startingTime").value;
+    var endingTime = document.getElementById("endingTime").value;
+    console.log(startingTime)
+    console.log(endingTime)
+    console.log(endingTime - startingTime);
+    function split(time) {
+        var t = time.split(":");
+        return t;
+    }
+    var start = split(startingTime);
+    var end = split(endingTime);
+    var hr = end[0] - start[0];
+    totalHr.value = hr;
+    balance.value = hr;
+})
