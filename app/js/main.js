5ea04252442b13e7be5b16a4c949e5502378de15
Guangqi Sun <sun357@alumni.purdue.edu>
2020-04-19 19:08:25 -0500

    Add negative check for time

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,25 @@
+const { ipcRenderer, dialog} = require('electron');
+
 var confirmBtn = document.getElementById("confirmBtn")
 var totalHr = document.getElementById("totalHr")
 var balance = document.getElementById("balance")
+
 confirmBtn.addEventListener('click', function() {
     var startingTime = document.getElementById("startingTime").value;
     var endingTime = document.getElementById("endingTime").value;
     console.log(startingTime)
     console.log(endingTime)
     console.log(endingTime - startingTime);
+    
+    if (endingTime < startingTime) {
+        ipcRenderer.send('time-error');
+        return;
+    }
     function split(time) {
         var t = time.split(":");
         return t;
     }
+
     var start = split(startingTime);
     var end = split(endingTime);
     var hr = end[0] - start[0];
